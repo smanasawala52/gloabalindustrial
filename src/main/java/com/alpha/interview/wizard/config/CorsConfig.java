e46d5ef4592532cfcd7fc801c2c85ab4654f4338
smanasawala52 <67199826+smanasawala52@users.noreply.github.com>
2024-05-27 13:03:04 +0530

    Update CorsConfig.java

--- src/main/java/com/alpha/interview/wizard/config/CorsConfig.java
+++ src/main/java/com/alpha/interview/wizard/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedOriginPatterns("*")
-				.allowedMethods("GET").allowedHeaders("*")
+				.allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE").allowedHeaders("*")
 				.allowCredentials(true);
 	}
 }
